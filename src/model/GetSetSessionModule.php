c19869e0f0a9bfa5a24e4c9a74d18fa6e88d467f
ArnaudFlorence <apr.florence@gmail.com>
2018-07-15 19:56:17 +0200

    GetSetSessionModule

---
+++ src/model/GetSetSessionModule.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('src/model/Connect.class.php');
+
+// Get advanced info on a specific session
+function getSessionModule($id) 
+{
+    $command =  "SELECT m.ID_MODULE, NOM_MODULE, s.ID_SESSION, NOM_SESSION".
+                 "FROM MODULE m, SESSION s, SESSION_MODULE sm".
+                 "WHERE m.ID_MODULE = sm.ID_MODULE".
+                 "AND s.ID_SESSION = sm.ID_SESSION";
+    $elanDb = new SQL_Connect();
+    // Create a new SQL_Connect object which is connecting to the Elan's Database
+    $elanDb->connect("Arnaud_ChampiBleu");
+    // Connect to the Arnaud_ChampiBleu database
+    $answer = $elanDb->ask($command);
+    // Execute the SQL command and return an array
+    return ($answer);
+}
+function setModule($id, $post)
+{
+    $command = "INSERT INTO SESSION_MODULE(ID_MODULE, ID_SESSION, NB_JOURS)".
+                " VALUES ('".$idModule."','" . $idSession . "','".$post['nb_Jours']."')";
+    echo $command;
+    $elanDb = new SQL_Connect();
+    // Create a new SQL_Connect object which is connecting to the Elan's Database
+    $elanDb->connect("Arnaud_ChampiBleu");
+    // Connect to the Arnaud_ChampiBleu database
+    $elanDb->set($command);
+    // Execute the SQL command and return an array
+}
+
+
+
