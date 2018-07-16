efdf3962434a4de6360b53f20b8692d5f1605a68
Unknown <apr.florence@gmail.com>
2018-07-16 09:39:51 +0200

    changement set et boucle pour form

--- src/controller/doForm.php
+++ src/controller/doForm.php
@@ -1,18 +1,17 @@
 <?php
 
 require_once('src/view/viewForm.php');
+require_once('src/view/viewNewEtudient.php');
 
 function doForm($id)
 {
-  // Appel de la fonction que tu dois implementer
-  viewForm(new view());
+if ($id==1){
+    viewForm(new view());
 }
-
-
-function doFormNewEtudient($id)
+else if ($id==2)
 {
   // Appel de la fonction que tu dois implementer
   viewNewEtudient(new view());
 }
 
-?>
+}
