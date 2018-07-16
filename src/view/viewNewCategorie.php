6b3594d16cd3e8e7720870f0ae2ea9729b10fc2b
Unknown <apr.florence@gmail.com>
2018-07-16 11:00:49 +0200

    form catégorie

--- src/view/viewNewCategorie.php
+++ src/view/viewNewCategorie.php
@@ -14,24 +14,10 @@ function viewNewCategorie($view)
     $view->addBody('<div>
             <aside>
                 <div>                   
-                    <form action="index.php?action=set&id=2" method="post" >
+                    <form action="index.php?action=set&id=3" method="post" >
 
-                        <h1>Inscription d\'un élève</h1><br/>
-                        <input placeholder="Prenom" type="text" name="PrenomStagiaire" /><br/>
-                        <input placeholder="Nom" type="text"  name="NomStagiaire" required/><br/>
-                        <input placeholder="Ville" type="text"  name="VilleStagiaire" required/><br/>
-                        <input type="email" name="EmailStagiaire" placeholder="E-mail ..." required/><br/>
-                        <input type="number" name="TelephoneStagiaire" placeholder="telephone" required/><br/>
-                        <input type="date" value="2000-12-25" name="DateNaissanceStagiaire" required/><br/>
-
-                        <label type=\'checkbox\'>
-                            Etes-vous :
-                            <select name="SexeStagiaire">
-                                <option value="0">Choisir...</option>
-                                <option value="M">Un Schtrumpf</option>
-                                <option value="F">Une Schtrumpfette</option>
-                            </select>
-                        </label><br/>
+                        <h1>Ajout d\'une catégorie</h1><br/>
+                        <input placeholder="nom de la catégorie" type="text" name="NomCategorie" /><br/>                    
                         <label for="CGU">Veuillez valider<input id="CGU" type="checkbox" name="CGU" required /></label><br/>
                         <input id="Valider" type="submit" value="Envoyer">
                         <input id="Annuler" type="reset" value="Annuler">
