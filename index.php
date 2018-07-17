d4dc2d3cb90e8f45ccd20f100166f99dc6f05fad
glorung <glorung@protonmail.com>
2018-07-17 13:57:59 +0200

    Fixing shit

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!ISSET($_GET['action'])) // Display standard WebPage
 {
         doSessionList();
 }
-else // Display Webpage related to $_GET['action']
+else // Display Webpage related to index.php?action=[action]
 {
     if ($_GET['action'] == "viewSessionDetail")
       doSessionDetail(ISSET($_GET['id']) ? $_GET['id'] : 3);
@@ -29,20 +29,20 @@ else // Display Webpage related to $_GET['action']
       doForm(ISSET($_GET['id']) ? $_GET['id'] : 1);
     }
     else if ($_GET['action'] == "set")
-    {   
+    {
         $id = ISSET($_GET['id']) ? $_GET['id'] : 1;
         if ($id == 1)
             setSession($_POST);
         else if ($id == 2)
-            setNewEtudient( $_POST);
+            setNewEtudient($_POST);
         else if ($id == 3)
-            setCategorie( $_POST);
-        else if ($id == 4)
-            getsetModule( $_POST);
-        else if ($id == 5)
-            getsetSessionModule( $_POST);
-        else if ($id == 6)
-            getsetSessionModule( $_POST);
+            setCategorie($_POST);
+        /* else if ($id == 4) */
+        /*     getsetModule($_POST); */
+        /* else if ($id == 5) */
+        /*     getsetSessionModule($_POST); */
+        /* else if ($id == 6) */
+        /*     getsetSessionModule($_POST); */
     }
     else
         echo "404 not found !";
